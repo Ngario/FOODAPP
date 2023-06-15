23f5c4031e279f8810b6d68186b920e11e6a088e
Idd Mumanyi <95312428+Ngario@users.noreply.github.com>
2023-06-15 06:33:20 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const App = () => {
+  return <div className="h-screen w-screen flex items-center justify-center text-blue-600">Idd</div>;
+  
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+
+import './index.css';
+import App from './App.js';
+
+ReactDOM.Render(
+    <App />, document.getElementById("root")
+);
